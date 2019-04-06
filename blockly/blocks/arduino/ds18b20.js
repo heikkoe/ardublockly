6994f1c0da82065cc14251001d01cbda3f81b063
Unknown <dajg1995@gmail.com>
2019-04-06 20:24:40 +0200

    fixed text

--- blockly/blocks/arduino/ds18b20.js
+++ blockly/blocks/arduino/ds18b20.js
@@ -33,18 +33,18 @@ Blockly.Blocks['ds18b20_config'] = {
     this.setHelpUrl('https://playground.arduino.cc/Learning/OneWire-DE');
     this.setColour(Blockly.Blocks.ds18b20.HUE);
     this.appendDummyInput()
-        .appendField(Blockly.Msg.ARD_STEPPER_SETUP) // Setup Temp. sensor
+        .appendField(Blockly.Msg.ARD_DS18B20_SETUP) // Setup temperatur sensor
         .appendField(
             new Blockly.FieldInstance('ds18b20',
-                                      Blockly.Msg.ARD_STEPPER_DEFAULT_NAME, // MyTempSensor
+                                      Blockly.Msg.ARD_DS18B20_DEFAULT_NAME, // MyTempSensor
                                       true, true, false),
             'DS18B20_NAME')
-        .appendField(Blockly.Msg.ARD_STEPPER_MOTOR); // :
+        .appendField(Blockly.Msg.ARD_DS18B20_SETUP2); // :
     this.appendDummyInput('PINS')
         .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField(Blockly.Msg.ARD_STEPPER_PIN1) // Pin
+        .appendField(Blockly.Msg.ARD_DS18B20_PIN) // pin
         .appendField(new Blockly.FieldDropdown(
             Blockly.Arduino.Boards.selected.digitalPins), 'DS18B20_PIN');
-    this.setTooltip(Blockly.Msg.ARD_STEPPER_SETUP_TIP); // Configures a temperature sensor
+    this.setTooltip(Blockly.Msg.ARD_DS18B20_SETUP_TIP); // Setup DS18B20 temperatur sensor
   }
 };
